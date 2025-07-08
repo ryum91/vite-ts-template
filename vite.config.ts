3dea4fc9764b0e761251f3203f647bc7a4d08620
ryum91 <ryum91@gmail.com>
2025-07-09 00:19:57 +0900

    fix: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()]
-});
+export default defineConfig(({ command }) => ({
+  plugins: [react(), tailwindcss()],
+  base: command === 'build' ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/` : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+}));
